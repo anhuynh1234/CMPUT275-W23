583a5eac9510e929103bad18b569294e1b8bcb93
An Huynh <thaianhuynh1234@outlook.com>
2023-03-29 18:54:34 -0600

    changes

---
+++ Lec18/worksheet/processor.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <unistd.h> 
+#include <fcntl.h> 
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <cstring>      // strerrno
+#include <errno.h>      // errno
+
+#define _MSG_MAX_LENGTH 100
+
+using namespace std;
+
+int main() {
+
+  const char * pipename = "./pipe";
+
+  // pipe is persumably created on the sender side, 
+  // so we just open it here for read-only access
+  int fd = open(pipename, O_RDONLY);
+  if ( fd == -1 ) {
+    // errno is set so we can check it
+    cout << "Pipe open failed" << endl;
+    cout << errno << ": " << strerror(errno) << endl << flush;
+    exit(1);
+  }
+  cout << "Named pipe opened for reading..." << endl;
+
+  char msg[_MSG_MAX_LENGTH];
+
+  // write your code here
+
+  // close pipe from the write end 
+  close(fd);
+
+  return 0;
+}
